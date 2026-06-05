ce8b9b45cf2dee3e5134d58f3ce6e50442c6c283
Fabrizzio Conrado <f.conrado88@hotmail.com>
2026-06-05 17:36:11 -0300

    Colocado botão para o peso do treino.

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ window.addEventListener('DOMContentLoaded', () => {
 inputPeso.addEventListener('input', () => {
     localStorage.setItem('peso', inputPeso.value);
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const btnSalvar = document.getElementById('btn-salvar-treino');
+    btnSalvar.value = localStorage.getItem('btnSalvarTreino') || '';
+});
+
+btnSalvar.addEventListener('click', () => {
+    localStorage.setItem('btnSalvarTreino', btnSalvar.value);
+
+    // Feedback visual para saber se deu certo.
+    alert('Treino salvo com sucesso!');
+});
